d996482098c3692ac57a3ce587d3a0bfe84b2025
Shoaib <shoaibchemistrysust@gmail.com>
2024-12-06 23:17:46 +0600

    donation for Feni completed

--- js/donation.js
+++ js/donation.js
@@ -22,4 +22,44 @@ addEventListener('click', function(){
 
     getUpdateBalance('input-noakhali', 'noakhali-balance', 'total-balance');
 
+}) 
+
+
+// donation for Feni
+
+document.getElementById('btn-donation-feni').
+addEventListener('click', function(){
+   
+    const donationFeni = getInputFieldValueById('input-feni');
+    const balanceMainAccount = getTextFieldValueById('total-balance');
+    
+    // validation input 
+    if(isNaN(donationFeni) || donationFeni<0 ||(donationFeni>balanceMainAccount)) {
+        alert('Failed donation');
+        return;
+    }
+
+    getUpdateBalance('input-feni', 'feni-balance', 'total-balance');
+
+}) 
+
+
+
+// donation for Noakhali
+
+document.getElementById('btn-donation-noakhali').
+addEventListener('click', function(){
+   
+    const donationNoakhali = getInputFieldValueById('input-noakhali');
+    const balanceMainAccount = getTextFieldValueById('total-balance');
+    
+    // validation input 
+    if(isNaN(donationNoakhali) || donationNoakhali<0 ||(donationNoakhali>balanceMainAccount)) {
+        alert('Failed donation');
+        return;
+    }
+
+
+    getUpdateBalance('input-noakhali', 'noakhali-balance', 'total-balance');
+
 })
